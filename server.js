1c2f066b60e1611f783cef8943e028d669e6b244
jns629 <jawwad621@gmail.com>
2021-03-17 19:57:16 +0500

    Version !.0

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+
+
+const app = express()
+const http = require('http').createServer(app)
+
+app.use(express.static(path.join(__dirname,'public')))
+
+const io = require('socket.io')(http)
+
+io.on('connection',socket=>{
+    console.log('connected ready');
+
+    socket.on('sendMessage',msg => {
+        // console.log(msg);
+        socket.broadcast.emit('sendToAll',msg)
+    })
+})
+
+
+const PORT = process.env.PORT || 3000
+
+http.listen(PORT,()=>{
+    console.log('server is running on port',PORT)
+
+})
